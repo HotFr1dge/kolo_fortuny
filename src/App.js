58f410bf3a3b448d53073ece03b3bbe1704261e0
Mateusz Skórka <skorka.mateusz@pm.me>
2025-01-23 17:00:00 +0100

    Some changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Password from './components/Password.js';
 
 function App() {
 
-  const [currentPassword, setCurrentPassword] = useState(0);
+  const [currentPasswordIndex, setCurrentPasswordIndex] = useState(0);
+  const [revealedLetters, setRevealedLetters] = useState([]);
 
   const passwords = [
     { password: 'KTO POD KIM DOŁKI KOPIE TEN WPADA', category: 'POWIEDZENIE' }
@@ -13,14 +14,61 @@ function App() {
 
   useEffect(() => {
 
-  }, []);
+    const handleKeyDown = (event) => {
+
+      // obłsuga tylko pojedyńczego wciśniecia
+      if (!event.repeat) {
+
+        const pressedLetter = event.key.toUpperCase();
+        const currentPassword = passwords[currentPasswordIndex].password;
+
+        if (/^[A-ZĄĆĘŁŃÓŚŹŻ]$/.test(pressedLetter)) {
+
+          if (currentPassword.includes(pressedLetter)) {
+
+            console.log(`Odkryto literę: ${pressedLetter}`);
+            setRevealedLetters((prev) => [...prev, pressedLetter]);
+
+          }
+          else {
+            console.log('Pudło!');
+
+            if (!document.body.classList.contains('failed')) {
+
+              document.body.classList.add('failed');
+
+              setTimeout(() => {
+                document.body.classList.remove('failed');
+              }, 1500)
+
+            }
+
+          }
+
+        }
+        else {  
+          console.log('Wciśnięto coś innego');
+        }
+
+
+
+      }
+
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [currentPasswordIndex, revealedLetters, passwords]);
 
   return (
     <div className="app">
-      <Password password={passwords[currentPassword].password} />
+      <Password password={passwords[currentPasswordIndex].password} revealedLetters={revealedLetters} />
 
       <div className="category">
-          {passwords[currentPassword].category}
+          {passwords[currentPasswordIndex].category}
       </div>
 
     </div>
--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Letter({ letter, space = false, isSelected = false, isRevealed  = false }) {
+export default function Letter({ letter, space = false, revealedLetters = [] }) {
+
+  const [isSelected, setIsSelected] = useState(false);
+  const [isRevealed, setisRevealed] = useState(false);
+
+  useEffect(() => {
+
+    if (revealedLetters.includes(letter.toUpperCase())) {
+      setIsSelected(true);
+    }
+
+  }, [revealedLetters]);
 
   return (
     <div
-      className={`letter ${space ? 'space' : ''} ${isSelected ? 'selected' : ''} ${isRevealed ? 'selected' : ''}`}
+      className={`letter ${space ? 'space' : ''} ${isSelected ? 'selected' : ''} ${isRevealed ? 'revealed' : ''}`}
     >
         <span className="content">
             {letter}
