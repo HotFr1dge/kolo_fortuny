94892d85b5ddae60577e9a860e7db2d6db6b1cbc
Mateusz Skórka <skorka.mateusz@pm.me>
2025-01-24 08:27:19 +0100

    Zmiany

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Password from './components/Password.js';
 function App() {
 
   const [currentPasswordIndex, setCurrentPasswordIndex] = useState(0);
+  const [currentPlayerId, setCurrentPlayerId] = useState(0);
+  const [userPoints, setUserPoints] = useState([0,0,0,0])
   const [revealedLetters, setRevealedLetters] = useState([]);
+  const [pointsToEarn, setPointsToEarn] = useState(0);
+  const [pointsMode, setPointsMode] = useState(false);
   const [selectedLetter, setSelectedLetter] = useState(false);
+  const [failed, setFaild] = useState(false);
 
   const passwords = useMemo(
     () => [
@@ -19,51 +24,96 @@ function App() {
   useEffect(() => {
 
     const handleKeyDown = (event) => {
+      if (event.repeat) return;
 
       const pressedLetter = event.key.toUpperCase();
+      const isLetter = /^[A-ZĄĆĘŁŃÓŚŹŻ]$/.test(pressedLetter);
+      const isNumber = /^[0-9]$/.test(pressedLetter);
+      const isSamogloska = /^[AEIOUYĄĘÓ]$/.test(pressedLetter);
       const currentPassword = passwords[currentPasswordIndex].password;
+      const shift = event.shiftKey;
 
-      // obłsuga tylko pojedyńczego wciśniecia
-      if (!event.repeat) {
+      if (pressedLetter === 'CAPSLOCK') {
 
-        if (/^[A-ZĄĆĘŁŃÓŚŹŻ]$/.test(pressedLetter) && !selectedLetter) {
+        if (pointsMode === false) {
+          setPointsToEarn('');
+        }
 
-          if (currentPassword.includes(pressedLetter)) {
+        setPointsMode((prev) => !prev);
 
-            console.log(`Odkryto literę: ${pressedLetter}`);
-            setSelectedLetter(pressedLetter);
-    
-          }
+        console.log('tryb wprowadzania');
+
+      }
+
+     if (pointsMode === true) {
+
+
+        if (isNumber)
+          setPointsToEarn((prev) => prev.toString() + pressedLetter);
+
+        return console.log('tryb wprowadzania: ' + pressedLetter);
+
+     }
+     
+     if (isLetter) {
+
+        if (revealedLetters.includes(pressedLetter)) {
+
+          (currentPlayerId + 1) >= 4 ? setCurrentPlayerId(0) : setCurrentPlayerId(currentPlayerId + 1);
+          setFaild(true);
+          setTimeout(() => {
+            setFaild(false);
+          }, 1500)
+
+        }
+        else if (currentPassword.includes(pressedLetter)) {
+
+          const audio = new Audio('letter_ok.mp3');
+          audio.play();
+
+          setRevealedLetters((prev) => [...prev, selectedLetter]);
+          setSelectedLetter(pressedLetter);
+
+          const updatedPoints = userPoints;
+          if (isSamogloska) {
+            updatedPoints[currentPlayerId] -= 300;
+          } 
           else {
-            console.log('Pudło!');
-
-            if (!document.body.classList.contains('failed')) {
-
-              document.body.classList.add('failed');
-
-              setTimeout(() => {
-                document.body.classList.remove('failed');
-              }, 1500)
-
-            }
-
+            updatedPoints[currentPlayerId] += Number(pointsToEarn);
           }
+          setUserPoints(updatedPoints);
+
+          console.log(userPoints);
 
         }
-        else {  
-          console.log('Wciśnięto coś innego');
+        else {
+
+          (currentPlayerId + 1) >= 4 ? setCurrentPlayerId(0) : setCurrentPlayerId(currentPlayerId + 1);
+          setFaild(true);
+          setTimeout(() => {
+            setFaild(false);
+          }, 1500)
+
         }
 
-      } else {
+      } 
+      else if (pressedLetter === ' ' && selectedLetter) {
 
-        if (pressedLetter === ' ' && /^[A-ZĄĆĘŁŃÓŚŹŻ]$/.test(selectedLetter)) {
           setRevealedLetters((prev) => [...prev, selectedLetter]);
           setSelectedLetter(false);
-        }
 
-        if (pressedLetter === 'ENTER') {
-          console.log('Odryj całość.');
-        }
+          const audio = new Audio('./reveal.mp3');
+          audio.play();
+
+      }
+      else if (shift && pressedLetter === ' ') {
+
+        const audio = new Audio('pass_solved.mp3');
+        audio.play();
+        
+        setRevealedLetters(currentPassword);
+
+        console.log('Odryj całość.');
 
       }
 
@@ -71,20 +121,37 @@ function App() {
 
     window.addEventListener("keydown", handleKeyDown);
 
-    console.log(selectedLetter);
-    console.log(revealedLetters);
-
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [currentPasswordIndex, revealedLetters, passwords, selectedLetter]);
+  }, [currentPasswordIndex, revealedLetters, passwords, selectedLetter, currentPlayerId, userPoints, pointsToEarn, pointsMode]);
 
   return (
-    <div className="app">
+    <div className={`app ${failed ? 'failed' : ''}`}>
+      <div className="header">
+        <h1>Koło fortuny</h1>
+        <p>Studniówka 2025</p>
+      </div>
+
       <Password password={passwords[currentPasswordIndex].password} revealedLetters={revealedLetters} selectedLetter={selectedLetter} />
 
       <div className="category">
           {passwords[currentPasswordIndex].category}
+      </div>
+
+      <div className="points-row">
+        <div className={`points-player ${currentPlayerId === 0 ? 'active' : ''}`}>
+          {Number(userPoints[0])}
+        </div>
+        <div className={`points-player ${currentPlayerId === 1 ? 'active' : ''}`}>
+          {Number(userPoints[1])}
+        </div>
+        <div className={`points-player ${currentPlayerId === 2 ? 'active' : ''}`}>
+          {Number(userPoints[2])}
+        </div>
+        <div className={`points-player ${currentPlayerId === 3 ? 'active' : ''}`}>
+          {Number(userPoints[3])}
+        </div>
       </div>
 
     </div>
